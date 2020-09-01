ed01656ac82375b16e120190196b1ad919b5b067
karenDluna <46972290+karenDluna@users.noreply.github.com>
2020-09-01 16:03:35 -0500

    actualizacionFinal

--- Assets/Scripts/FlyFlappy.cs
+++ Assets/Scripts/FlyFlappy.cs
@@ -7,7 +7,7 @@ public class FlyFlappy : MonoBehaviour
     // Start is called before the first frame update
     //controles del pajaro
     //saltar
-
+    public GameManager gameManager;
     [SerializeField] public float velocity = 1;
     private Rigidbody2D _rigidbody2D;
     void Start()
@@ -23,5 +23,9 @@ public class FlyFlappy : MonoBehaviour
             //salto
             _rigidbody2D.velocity = Vector2.up * velocity;
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        gameManager.GameOver();
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject GameOverCanvas;
+    void Start()
+    {
+       
+       Time.timeScale = 1;
+       GameOverCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    public void GameOver()
+    {
+        GameOverCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void Replay()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
