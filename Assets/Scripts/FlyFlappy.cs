dc274f52202ef4767aedefec228a28bf66aa2fef
karenDluna <46972290+karenDluna@users.noreply.github.com>
2020-09-01 13:41:45 -0500

    Commit1092020

---
+++ Assets/Scripts/FlyFlappy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyFlappy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //controles del pajaro
+    //saltar
+
+    [SerializeField] public float velocity = 1;
+    private Rigidbody2D _rigidbody2D;
+    void Start()
+    {
+         _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //salto
+            _rigidbody2D.velocity = Vector2.up * velocity;
+        }
+    }
+}
---
+++ Assets/Scripts/PipeSpawn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]public float maxTime = 3;
+    private float _timer = 0;
+    public GameObject Pipe;
+
+    [SerializeField]public float height ;
+    //[SerializeField]public double TopHeight = 3;
+    //[SerializeField]public double BottomHeight = 3;
+
+    // This will create the spawning of the pipes
+    void Start()
+    {
+        GameObject newpipe = Instantiate(Pipe);
+        newpipe.transform.position = transform.position + new Vector3(0, Random.Range(-height,(height+1)),0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_timer > maxTime)
+        {
+            GameObject newpipe = Instantiate(Pipe);
+            newpipe.transform.position = transform.position + new Vector3(0, Random.Range(-height,(height+1)),0);
+            Destroy(newpipe,15);
+            _timer = 0;
+        }
+
+        _timer += Time.deltaTime;
+    }
+    
+}
